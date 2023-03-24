2a1db880f054577f3cb52ae9ed1de7d20d1ecaa4
adailtondev <adailtonh.dev@gmail.com>
2023-03-24 09:03:27 -0300

    Fiz o link sobre

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendendo react
-        </a>
-      </header>
-    </div>
+    <a href='#to-do' className='sobre'>Sobre</a>
   );
 }
 
