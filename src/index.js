3c37f6bfe18e2a2689e54ee920360e689188f65e
adailtondev <adailtonh.dev@gmail.com>
2023-03-24 09:40:41 -0300

    arquitetura de pastas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/settings/colors.css'
+import './styles/generic/reset.css'
+import './styles/elements/base.css'
 import './index.css';
 import App from './App';
 
