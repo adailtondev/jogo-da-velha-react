c68e59fcacbd1ae8161ae374637a57d83d224a92
adailtondev <adailtonh.dev@gmail.com>
2023-03-30 15:14:01 -0300

    aceitando dois jogadores

--- src/objects/Player/index.js
+++ src/objects/Player/index.js
@@ -1,7 +1,18 @@
 import React from 'react'; 
+import './Player.css'
+import playerX from '../../img/X.png'
+import playerO from '../../img/O.png'
 
-const Player = () => (
-    <button className='player'>X</button>
-);
+const Player = (props) => {
+    const players = [];
+    players['x'] = playerX;
+    players['o'] = playerO;
+
+    return (
+    <button className='player'>
+        <img src={players[props.player]} alt={`Jogador ${props.player.toUpperCase()}`}/>
+    </button>
+    )
+};
 
 export default Player;
